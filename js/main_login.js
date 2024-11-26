325f039c2eb4eabc85d8f35591267702b76bdcbe
Stefano Sanchez <stefanosanchez8@gmail.com>
2024-11-25 21:11:23 -0300

    Se agrega nuevo perfil basico y vista

--- js/main_login.js
+++ js/main_login.js
@@ -1,13 +1,16 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    if (username === "admin" && password === "admin") {
-      alert("Inicio de sesión exitoso");
-      window.location.href = "main.html"; // Redireccionar a la página principal
-    } else {
-      alert("Usuario o contraseña incorrectos");
-    }
-  });
+  event.preventDefault();
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "Admin1234") {
+    alert("Inicio de sesión exitoso");
+    window.location.href = "main.html"; // Redireccionar a la página principal
+  } else if (username === "cliente" && password === "Cliente1234") {
+    alert("Inicio de sesión básico exitoso");
+    window.location.href = "basico.html"; // Redireccionar a la página básica
+  } else {
+    alert("Usuario o contraseña incorrectos");
+  }
+});
