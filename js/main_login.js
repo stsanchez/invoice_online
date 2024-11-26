3205e17c5cdf2a2785a6f5cd46fa59bad297b158
Stefano Sanchez <stefanosanchez8@gmail.com>
2024-11-25 21:18:01 -0300

    toLoweCase en usuario

--- js/main_login.js
+++ js/main_login.js
@@ -1,13 +1,13 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
   event.preventDefault();
 
-  var username = document.getElementById("username").value;
+  var username = document.getElementById("username").value.toLowerCase(); // Convertir a minúsculas
   var password = document.getElementById("password").value;
 
   if (username === "admin" && password === "Admin1234") {
     alert("Inicio de sesión exitoso");
     window.location.href = "main.html"; // Redireccionar a la página principal
-  } else if (username === "cliente" && password === "Cliente1234") {
+  } else if (username === "usuario_basico" && password === "Admin1234") {
     alert("Inicio de sesión básico exitoso");
     window.location.href = "basico.html"; // Redireccionar a la página básica
   } else {
