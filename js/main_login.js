2b95843718b10bdcc8fe50cd050a71b7a4009c50
Stefano Sanchez <stefanosanchez8@gmail.com>
2024-11-25 21:36:17 -0300

    usuario cliente

--- js/main_login.js
+++ js/main_login.js
@@ -7,7 +7,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
   if (username === "admin" && password === "Admin1234") {
     alert("Inicio de sesión exitoso");
     window.location.href = "main.html"; // Redireccionar a la página principal
-  } else if (username === "usuario_basico" && password === "Admin1234") {
+  } else if (username === "cliente" && password === "Cliente1234") {
     alert("Inicio de sesión básico exitoso");
     window.location.href = "basico.html"; // Redireccionar a la página básica
   } else {
